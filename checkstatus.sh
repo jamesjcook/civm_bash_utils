b6c8eb69f873f97e03f5963caf43e07ec44303a2
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2010-04-01 13:50:02 +0000

    civmlibrary updated again
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/civmlibrary/trunk@254 d41079a3-eaa1-4f09-8506-b324c47726bb

--- checkstatus.sh
+++ checkstatus.sh
@@ -11,10 +11,20 @@
 name=`whoami`
 host=`hostname -s`
 
-file=` ls configs/*${host}*${name}*plist `
+if [`ls configs/*plist | wc -l` == "1"]
+then
+    file=` ls configs/*plist`  # One config to rule them all.
+elif [`ls configs/*${host}*plist | wc -l` == "1"]
+    file=`ls configs/*${host}*plist` # each host has a different config
+else
+    file=`ls configs/*${host}*${name}*plist | wc -l` #each user on a host has a different config
+then
+fi
+
 file=`basename $file`
 
 echo project plist is $file 
+diff -yq --suppress-common-lines configs/$file /Library/LaunchDaemons/$file
 echo Project launchctl entry 
 sudo launchctl list | grep ${file%\.*} 
 echo project launchd control file contents
--- stopscheduler.sh
+++ stopscheduler.sh
@@ -11,7 +11,16 @@
 name=`whoami`
 host=`hostname -s`
 
-file=` ls configs/*${host}*${name}*plist `
+if [`ls configs/*plist | wc -l` == "1"]
+then
+    file=` ls configs/*plist`  # One config to rule them all.
+elif [`ls configs/*${host}*plist | wc -l` == "1"]
+    file=`ls configs/*${host}*plist` # each host has a different config
+else
+    file=`ls configs/*${host}*${name}*plist | wc -l` #each user on a host has a different config
+then
+fi
+
 file=`basename $file`
 
 echo stopping ${file%.*} 
