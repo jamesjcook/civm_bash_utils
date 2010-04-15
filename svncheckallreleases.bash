1d8e7b7f7f86719726902718ee45fd0228ebd4ea
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2010-04-15 17:54:17 +0000

    Civmlibrary updaated svn status scripts
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/civmlibrary/trunk@370 d41079a3-eaa1-4f09-8506-b324c47726bb

--- svncheckallreleases.bash
+++ svncheckallreleases.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # checks all the folders in the current directory to se if they need to be re-released
+# assumes that the current directory is full of svn projects in the standard style
 for folder in `ls -d */` 
 do
     echo $folder checking trunk against tags
--- svncommitall.sh
+++ svncommitall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # quck script to commit all with basic comment of dirname update
+# asusmes that all directories in the current folder are svn directories and could use comiting. 
+# svn will ignore directories which dont need comits.
 for file in `ls -d */` 
 do 
     echo $file
