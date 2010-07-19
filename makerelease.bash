0b9c47e54447bc2c16571bc241c0ad26bee80db9
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2010-07-19 20:27:16 +0000

    civmlibrary polished off makerelease script
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/civmlibrary/trunk@497 d41079a3-eaa1-4f09-8506-b324c47726bb

--- makerelease.bash
+++ makerelease.bash
@@ -4,12 +4,58 @@
 # should do a quick check version, followed by a svn export, should it add back to svn?....?
 
 
+STARTDIR=$PWD
+SCRIPTNAME=`basename $STARTDIR`
+. $STARTDIR/lib/functionscivmscript.bash
+
+releasedir="/Users/Shared/civmscriptreleases" # reasonable default?
+
+version=`ls $STARTDIR | grep -E "^v[0-9]+(\.?[0-9]+)*"`
+# if versions is an array error
+svnurl=`svn info | grep URL | cut -d ":" -f 2- | cut -d" " -f 2-`
+
+svnproj=${svnurl%/*}
+REPOSURL=${svnproj%/*}
+projectname=${svnproj##*/}
+tagdir="${svnurl%/*}/tags"
+#echo $REPOSURL/$projectname
+
+tags=`svn list $tagdir | cut -d "/" -f 1`
+#echo $tags
+#space seperated list
+index=0
+
+echo "
+$REPOSURL
+$projectname
+tags
+$version"
+
+#exit 
 ./checktrunkvstags.sh  # make sure release is current.
 if [ $? -eq "0" ] 
 then
-    svn export ${REPOSURL}/${project}/tags/${version} ${project}_${version}
-    tar -xzf ${project}_${version} ${project}_${version}.tar.gz 
-#... svn add ${project}_${version}.tar.gz ${REPOSURL}/${project}/releases?
+    if [ ! -d "${releasedir}/${projectname}_${version}" -a ! -e "${releasedir}/${projectname}_${version}.tar.gz" ]
+    then
+	svn export "${REPOSURL}/${projectname}/tags/${version}" "${releasedir}/${projectname}_${version}"
+	tar -czf "${releasedir}/${projectname}_${version}.tar.gz" "${releasedir}/${projectname}_${version}"
+	read -p "Do you wish to remove ${releasedir}/${projectname}_${version} [Y/n]" -n 1 removetrue
+	if [ "$removetrue"=="n" -o "$removetrue"="N" ]
+	then
+	    echo ""
+	    echo "WARNING:  ${releasedir}/${projectname}_${version} not removed, "
+	    echo "          you will not be able to run this release again until it is delted"
+	else
+	    rm -rf "${releasedir}/${projectname}_${version}"
+	fi
+    else 
+	echo "ERROR:  previously released, or release previously attempted. Delete directory and tar of last attempt.
+    ${releasedir}/${projectname}_${version}
+    ${releasedir}/${projectname}_${version}.tar.gz"
+	exit 1
+    fi
+    echo "STATUS:  Release tar is ${projectname}_${version}.tar.gz in ${releasedir}/"
+#... svn add ${releasedir}/${projectname}_${version}.tar.gz ${REPOSURL}/${projectname}/releases?
 else
     echo "checktrunkvstags failed, fix problem and retry."
 fi
