c7ca9482b1c15454eeac4425338be6ab1b57ce19
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2014-01-21 20:13:36 +0000

    svnbranch behavior updated and rather functional
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/civmlibrary/trunk@1856 d41079a3-eaa1-4f09-8506-b324c47726bb

--- svnbranch.sh
+++ svnbranch.sh
@@ -10,15 +10,18 @@ SCRIPTNAME=`basename $STARTDIR`
 version=`ls $STARTDIR | grep -E "^v[0-9]+(\.?[0-9]+)*"`
 # if versions is an array error
 initialurl=`svn info | grep URL | cut -d ":" -f 2-`
-`echo $initialurl`  | grep -c tags
-tagurlstatus=$?
-if [ $tagurlstatus ] 
+tagurlstatus=`echo $initialurl | grep -c tags`
+trunkurlstatus=`echo $initialurl | grep -c trunk`
+if [  "$tagurlstatus" == 1 ]
 then
-    tagdir=`"${initialurl%/*}`
-    projecturlbase=`"${tagurl%/*}`
+    echo "this url is in a tag"
+    tagurl="${initialurl%/*}"
+    projecturlbase="${tagurl%/*}"
+else
+    projecturlbase="${initialurl%/*}"    
+    tagurl="${projecturlbase}/tags"
 fi
-tagdir="${projecturlbase%/*}/tags"
-branchdir="${projecturlbase%/*}/branches"
+branchurl="${projecturlbase}/branches"
 
 branchname=$1
 if [ -z $branchname ]
@@ -27,8 +30,8 @@ then
     exit
 fi
 
-
-tags=`svn list $tagdir | cut -d "/" -f 1`
+echo "Getting tags using command: svn list $tagurl"
+tags=`svn list $tagurl | cut -d "/" -f 1`
 #space seperated list
 index=0
 for tag in $tags
@@ -41,33 +44,44 @@ lastindex=${#tagarray[*]}
 
 ((lastindex=$lastindex-1))
 lasttag=${tagarray[$lastindex]}
-
 isversioncomited=`svn status $version`
+
+if [  "$tagurlstatus" == 1 ]
+then
+    branchname="${version}_"$branchname
+elif [ "$trunkurlstatus" == 1 ]
+then
+    branchname="trunk_"$lasttag"_"$branchname
+fi
 
 
 if [ -z "$isversioncommited" ]
 then
-    echo Found base url: $projecturlbase
-    echo Tag dir url: $tagdir
-    echo Branch dir url: $branchdir
-    echo Found version: $version
-    echo Found tags: $tags
-    echo New branch name:$branchname
+    echo "Found base url: "$projecturlbase
+    echo "Tag dir url:    "$tagurl
+    echo "Branch dir url: "$branchurl
+    echo "Found version:  "$version
+    echo "Found tags:     "$tags
+    echo "New branch name:"$branchname
 #echo Tag array: ${tagarray[*]}
 #echo last index is $lastindex
     if [ "$lasttag" != "$version" ] 
     then # prompt for create tag
 	echo last tag was: $lasttag which is not $version
-	read -n 1 -p "Would you like to branch to: $branchname [y/N]
-svn cp $initialurl $branchdir/$branchname     "  OPTION
-	echo .
-	if [ "$OPTION" == "y" -o "$OPTION" == "Y" ]
-	then
-	    svn cp $projecturlbase $branchdir/$branchname
-	fi
+	echo "  maybe you should tag this before your branch"
     else
 	echo last release version $lasttag upto date with version $version
     fi
+    read -n 1 -p "Would you like to branch to: $branchname [y/N]
+svn cp $initialurl $branchurl/$branchname
+svn switch $branchurl/$branchname     "  OPTION
+    echo .
+    if [ "$OPTION" == "y" -o "$OPTION" == "Y" ]
+    then
+	svn cp $initialurl $branchurl/$branchname
+	svn switch $branchurl/$branchname
+    fi
+
 else 
     echo "Version: $version has not been commited yet, please commit and re run $0"
     exit 1
